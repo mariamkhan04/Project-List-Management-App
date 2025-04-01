ba7ed25ccbb1e05094c9c6a08638a8362c154649
mariamkhan04 <125134327+mariamkhan04@users.noreply.github.com>
2025-04-02 00:11:56 +0500

    updated backend url

--- client/src/api/projectService.jsx
+++ client/src/api/projectService.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://project-list-management-app-api.vercel.app/api';
 
 export const fetchProjects = async () => {
   const response = await fetch(`${API_BASE_URL}/projects`);
