1fe37cd3b652621dea013a5eb3a503093d5b2369
mariamkhan04 <125134327+mariamkhan04@users.noreply.github.com>
2025-04-01 23:45:16 +0500

    installed dotenv

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config({
+    path: '.env'
+})
 
 const app = express();
 const PORT = process.env.PORT || 5000;
